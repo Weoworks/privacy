a5b1dddb6a9b30d10fad440a43ba5fd53e1ef3a0
Weoworks <108215869+Weoworks@users.noreply.github.com>
2024-04-06 16:38:40 +0800

    Update fakeping.sh

--- fakeping.sh
+++ fakeping.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 检查路由文件中是否包含 "geoip:private"
-check=$(jq '.rules[].ip[]' /etc/XrayR/route.json | grep -o '"geoip:private"')
+check=$(grep -o '"geoip:private"' /etc/XrayR/route.json)
 
 echo -e "我知道设置这个没用，但是我还是设置了"
 read -p "您确定要设置本服务器吗？(Y/N): " yorn
@@ -10,8 +10,8 @@ if [[ "${yorn}" == "y" || "${yorn}" == "Y" ]]; then
     echo -e "127.0.0.1 gstatic.com" | sudo tee -a /etc/hosts >/dev/null
     echo -e "127.0.0.1 gstatic.com" | sudo tee -a /etc/hosts >/dev/null
     if [ -n "$check" ]; then
-        # 使用 jq 工具替换 JSON 文件中的内容
-        jq '.rules[0].outboundTag = "IPv4_out"' /etc/XrayR/route.json
+        # 使用 sed 替换 JSON 文件中的内容
+        sed -i '6s/.*/      "outboundTag": "IPv4_out",/' /etc/XrayR/route.json
     fi
     echo -e "添加成功~"
 else
