31d085a6a5df8d78759dbf98530748bd3d332a70
Weoworks <108215869+Weoworks@users.noreply.github.com>
2024-04-06 12:06:18 +0800

    Update fakeping.sh

--- fakeping.sh
+++ fakeping.sh
@@ -2,9 +2,9 @@
 echo -e "我知道设置这个没用，但是我还是设置了"
 read -p "您确定要设置本服务器吗？(Y/N):" yorn
 if [[ x"${yorn}" == x"y" || x"${yorn}" == x"Y" ]]; then
-echo -e "127.0.0.1 gstatic.com" >> /etc/hosts
-echo -e "127.0.0.1 gstatic.com" >> /etc/hosts
-echo -e "添加成功~"
+    echo -e "127.0.0.1 gstatic.com" >> /etc/hosts
+    echo -e "127.0.0.1 gstatic.com" >> /etc/hosts
+    echo -e "添加成功~"
 else
-   echo -e "正在关闭..."
+    echo -e "正在关闭..."
 exit
