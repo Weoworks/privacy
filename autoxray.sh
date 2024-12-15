ebe092877c66305f5976b637b4d17919c147458c
Weoworks <108215869+Weoworks@users.noreply.github.com>
2024-12-15 10:06:44 +0800

    Create autoxray.sh

---
+++ autoxray.sh
@@ -0,0 +1,10 @@
+mkdir /etc/autoxray
+cd /etc/autoxray
+wget https://raw.githubusercontent.com/Weoworks/privacy/refs/heads/main/shadowsocks.json
+wget https://github.com/XTLS/Xray-core/releases/download/v24.11.30/Xray-linux-64.zip
+unzip Xray-linux-64.zip
+wget https://raw.githubusercontent.com/Weoworks/privacy/refs/heads/main/autoxray.service
+mv autoxray.service /etc/systemd/system/autoxray.service
+systemctl enable autoxray
+systemctl start autoxray
+echo "运行完成"
