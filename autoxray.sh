f2c3af6e03bcf659c4c8607fd3830c45100cd3c2
Weoworks <108215869+Weoworks@users.noreply.github.com>
2024-12-24 22:14:07 +0800

    Update autoxray.sh

--- autoxray.sh
+++ autoxray.sh
@@ -3,7 +3,7 @@ cd /etc/autoxray
 wget https://raw.githubusercontent.com/Weoworks/privacy/refs/heads/main/shadowsocks.json
 mv shadowsocks.json config.json
 wget https://github.com/XTLS/Xray-core/releases/download/v24.11.30/Xray-linux-64.zip
-apt uodate && apt install unzip -y
+apt update && apt install unzip -y
 unzip Xray-linux-64.zip
 wget https://raw.githubusercontent.com/Weoworks/privacy/refs/heads/main/autoxray.service
 mv autoxray.service /etc/systemd/system/autoxray.service
